b69c5c18e1012d3a1279d60112e662c2d7a3e4d5
Глеб Кононов <glebkononov@MacBook-Air-Gleb.local>
2023-09-08 19:29:55 +0300

    first version of app

---
+++ app/src/main/java/com/example/testmacapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.testmacapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a = 5;
+        int b = 10;
+        int c = Integer.parseInt(String.valueOf(a+b));
+        Log.d("MainActivity", String.valueOf(c));
+    }
+}
